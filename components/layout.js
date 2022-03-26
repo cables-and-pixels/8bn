864c0c94bd9553d7f6f6fe78bd656e1165a48b43
Laurent Houdard <laurent@houdard.net>
2022-03-26 23:00:26 +0100

    Footer

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,7 @@
 import Container from 'react-bootstrap/Container';
+import Link from 'next/link';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Layout({ children }) {
   return (
@@ -8,6 +11,36 @@ export default function Layout({ children }) {
           {children}
         </Container>
       </main>
+      <footer>
+        <Container>
+          <Row>
+            <Col md={6}>
+              <p>
+                © 2022{' '} cables.and.pixels
+                {' | '}
+                <Link href="/profile/tz1WwNyzXEGvSxZDEvugnDghcsQabxHWcsvn">
+                  <a>Creations</a>
+                </Link>
+                {' | '}
+                <a href="https://twitter.com/CablesAndPixels">
+                  Twitter
+                </a>
+              </p>
+            </Col>
+            <Col md={6}>
+              <p className="text-end">
+                Other resources:{' '}
+                <a href="https://www.8bidou.com/">
+                  8bidou.com
+                </a>{' | '}
+                <a href="https://8bidou-tools.vercel.app/">
+                  8bidou pinkyblu's tools
+                </a>
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
     </>
   );
 }
