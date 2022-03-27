cfb1045ffeb6c00c10ae2bb676648781bf700352
Laurent Houdard <laurent@houdard.net>
2022-03-27 01:00:04 +0100

    Footer

--- components/layout.js
+++ components/layout.js
@@ -16,24 +16,26 @@ export default function Layout({ children }) {
           <Row>
             <Col md={6}>
               <p>
-                © 2022{' '} cables.and.pixels
-                {' | '}
+                © 2022{' '} cables.and.pixels<br />
                 <Link href="/profile/tz1WwNyzXEGvSxZDEvugnDghcsQabxHWcsvn">
                   <a>Creations</a>
                 </Link>
                 {' | '}
-                <a href="https://twitter.com/CablesAndPixels">
+                <a href="https://twitter.com/CablesAndPixels"
+                   target="_blank" rel="noreferrer">
                   Twitter
                 </a>
               </p>
             </Col>
             <Col md={6}>
-              <p className="text-end">
-                Other resources:{' '}
-                <a href="https://www.8bidou.com/">
+              <p className="text-md-end">
+                Other resources<br />
+                <a href="https://www.8bidou.com/"
+                   target="_blank" rel="noreferrer">
                   8bidou.com
                 </a>{' | '}
-                <a href="https://8bidou-tools.vercel.app/">
+                <a href="https://8bidou-tools.vercel.app/"
+                   target="_blank" rel="noreferrer">
                   8bidou pinkyblu&#39;s tools
                 </a>
               </p>
