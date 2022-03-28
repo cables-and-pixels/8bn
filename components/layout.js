3b3d7f6ca66cdd13d4d34745fe4f94125e842729
Laurent Houdard <laurent@houdard.net>
2022-03-28 10:07:49 +0200

    Footer

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faImage, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 export default function Layout({ children }) {
@@ -15,38 +15,34 @@ export default function Layout({ children }) {
         </Container>
       </main>
       <footer>
-        <Container>
-          <Row>
-            <Col md={6}>
-              <p>
-                © 2022{' '} cables.and.pixels<br />
-                <Link href="/profile/tz1WwNyzXEGvSxZDEvugnDghcsQabxHWcsvn">
-                  <a>Creations</a>
-                </Link>
-                {' | '}
-                <a href="https://twitter.com/CablesAndPixels"
-                   target="_blank" rel="noreferrer">
-                  <FontAwesomeIcon icon={faTwitter} />{' '}
-                  Twitter
-                </a>
-              </p>
-            </Col>
-            <Col md={6}>
-              <p className="text-md-end">
-                Other resources<br />
-                <a href="https://www.8bidou.com/"
-                   target="_blank" rel="noreferrer">
-                  8bidou.com
-                  {' '}<FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                </a>{' | '}
-                <a href="https://8bidou-tools.vercel.app/"
-                   target="_blank" rel="noreferrer">
-                  8bidou pinkyblu&#39;s tools
-                  {' '}<FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                </a>
-              </p>
-            </Col>
-          </Row>
+        <Container className="text-end">
+          <p>
+            Alternative browsing tool by<br />
+            <Link href="/profile/tz1WwNyzXEGvSxZDEvugnDghcsQabxHWcsvn">
+              <a>
+                cables.and.pixels
+                {' '}<FontAwesomeIcon icon={faImage} />
+              </a>
+            </Link>
+            {' | '}
+            <a href="https://twitter.com/CablesAndPixels"
+               target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+          </p>
+          <p>
+            Other resources<br />
+            <a href="https://www.8bidou.com/"
+               target="_blank" rel="noreferrer">
+              8bidou.com
+              {' '}<FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </a>{' | '}
+            <a href="https://8bidou-tools.vercel.app/"
+               target="_blank" rel="noreferrer">
+              8bidou pinkyblu&#39;s tools
+              {' '}<FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </a>
+          </p>
         </Container>
       </footer>
     </>
